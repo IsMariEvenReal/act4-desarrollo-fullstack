b66dbf98b290cf9527b79e12b65b66c89dde3aa2
IsMariEvenReal <guajardomariajose7@gmail.com>
2026-02-28 16:49:26 -0600

    Add files via upload

--- db.js
+++ db.js
@@ -7,10 +7,10 @@ const mysql = require('mysql2');
 
 //Se crea conexión
 const pool = mysql.createPool({
-  host: process.env.TIDB_HOST,
-  user: process.env.TIDB_USER,
-  password: process.env.TIDB_PASSWORD,
-  database: process.env.TIDB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: 4000,
   ssl: { minVersion: 'TLSv1.2',
     rejectUnauthorized: false },
