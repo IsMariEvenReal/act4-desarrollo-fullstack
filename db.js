51a77b6bcfadb7e8bf6ebc34a396ae34f736d7b8
IsMariEvenReal <guajardomariajose7@gmail.com>
2026-02-28 16:28:05 -0600

    Add files via upload

--- db.js
+++ db.js
@@ -6,26 +6,24 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 //Se crea conexión
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,     // Lee host desde el .env
-    user: process.env.DB_USER,     // Lee usuario (de la dueña de la base de datos) desde el .env
-    password: process.env.DB_PASS, // Lee contraseña desde el .env
-    database: process.env.DB_NAME, // Lee el nombre de la base de datos
-    port: process.env.DB_PORT || 4000,
-    ssl: {
-        minVersion: 'TLSv1.2',
-        rejectUnauthorized: true // TiDB recomienda esto para seguridad
-    }
+const pool = mysql.createPool({
+  host: process.env.TIDB_HOST,
+  user: process.env.TIDB_USER,
+  password: process.env.TIDB_PASSWORD,
+  database: process.env.TIDB_NAME,
+  port: 4000,
+  ssl: { rejectUnauthorized: false },
+  connectionLimit: 10 // El Pool mantiene hasta 10 conexiones listas
 });
 
-if (process.env.NODE_ENV !== 'test') {
-    connection.connect((err) => {
+/* if (process.env.NODE_ENV !== 'test') {
+    pool.connect((err) => {
         if (err) {
             console.error('Error conectando a MySQL: ' + err.stack);
             return;
         }
         console.log('Conectado a MySQL con éxito');
     });
-}
+} */
 
-module.exports = connection;
+module.exports = pool;
