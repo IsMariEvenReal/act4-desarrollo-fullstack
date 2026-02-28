f42ee8d273e6d61432a25ddccadd65e8bdd85a6c
IsMariEvenReal <guajardomariajose7@gmail.com>
2026-02-28 17:12:01 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
     next();
 });
 
